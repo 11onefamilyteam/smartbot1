239d7f7c4b91242d0607ed2c07aaf024ecfa8018
11onefamilyteam <44616077+11onefamilyteam@users.noreply.github.com>
2018-10-30 21:14:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,8 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "."
 const devs = ['200644160179535872','455653515881218048'];
-var prefix = ".";
 const adminprefix = "."
-const db = require('quick.db');
-const client = new Discord.Client();   
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-const canvas = require("canvas");
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map(); 
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
-const dateFormat = require('dateformat'); 
-const pretty = require('pretty-ms') 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
